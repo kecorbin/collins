dac82bc9dfb38bcc06e53f701d8d0837c7d9c609
kecorbin <kecorbin@cisco.com>
2017-04-17 14:48:06 -0500

    code cleanup

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ from django_celery_beat.models import PeriodicTask
 
 safe_tags = html_cleaner.HTMLCleaner(allow_tags=['a', 'img', 'em', 'strong'])
 
+
 class Environment(models.Model):
     """
     Base class for info passed to the plugin
@@ -19,24 +20,8 @@ class Environment(models.Model):
     class Meta:
         unique_together = ('name', 'description', 'json')
 
-# class FabricTarget(models.Model):
-#     """
-#     a  FabricTarget represents an ACI we will run tests against
-#     """
-#     plugin_prefix = models.CharField(max_length=25, default="aci")
-#     APIC_LOGIN = models.CharField(max_length=25, default='admin')
-#     APIC_PASSWORD = models.CharField(max_length=25)
-#     APIC_URL = models.URLField(max_length=25)
-#
-#     class Meta:
-#         unique_together = ('APIC_LOGIN', 'APIC_PASSWORD', 'APIC_URL')
-#
-#     def __str__(self):
-#         return self.fabric_name
-
 
 class Job(PeriodicTask):
-    # name = models.CharField(max_length=25)
     type = models.CharField(max_length=25, default="DockerJob")
     last_result = models.CharField(max_length=10, default="Fail")
     environment = models.ForeignKey(Environment, blank=True, null=True, default=None)
@@ -57,6 +42,3 @@ class Result(models.Model):
     json = jsonfield.JSONField(max_length=512, null=True, blank=False)
     pluginHTMLResponse = HTMLField(safe_tags, null=True, blank=False)
     pluginRawResponse = models.TextField(blank=True, null=True)
-
-
-

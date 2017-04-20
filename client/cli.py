ce4de7d833d305e97740a3af142424031e03bfd9
kecorbin <kecorbin@cisco.com>
2017-04-20 15:42:36 -0500

    all my latest commits

---
+++ client/cli.py
@@ -0,0 +1,14 @@
+# can be pasted into interpreter for an interactive session with the broker/backend
+import docker
+from celery import Celery
+import os
+
+
+app = Celery('tasks', broker=os.getenv('RABBITMQ_URL'),
+                      backend=os.getenv("REDIS_URL"),
+                      includes=['api.tasks']
+             )
+
+import api.tasks
+
+results = api.tasks.run_image.apply_async(args=['docker/whalesay'], kwargs={"command":"cowsay moo"}, queue="SITE_1")

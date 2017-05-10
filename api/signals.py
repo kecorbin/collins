4265744e2a74aaf8ca70780c9d4029c6147293e1
Josh Anderson <joshand@cisco.com>
2017-05-09 23:12:05 -0500

    Fix signals to only set restart flag on DockerJob creation (not updates)

--- api/signals.py
+++ api/signals.py
@@ -3,11 +3,11 @@ from django.dispatch import receiver
 from models import DockerJob, Scheduler
 
 
-@receiver(post_save)
-def set_scheduler_restart_flag(sender, instance=None, **kwargs):
-    print("trying to set restart flag")
-    obj = Scheduler.objects.get(id=1)
-    print("Was:", obj.restart)
-    Scheduler.objects.select_related().filter(id=1).update(restart=True)
-    print("Now:", obj.restart)
-    #obj.save(obj)
+@receiver(post_save, sender=DockerJob)
+def set_scheduler_restart_flag(sender, instance=None, created=False, **kwargs):
+    if created:
+        print("trying to set restart flag; sender=", sender, "created=", created)
+        obj = Scheduler.objects.get(id=1)
+        print("Was:", obj.restart)
+        Scheduler.objects.select_related().filter(id=1).update(restart=True)
+        print("Now:", obj.restart)

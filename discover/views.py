7c4df689bd47b8ba55005c83d14477ab8bbf2dda
kecorbin <kecorbin@cisco.com>
2017-06-14 10:30:23 -0500

    discover views cleanup

--- discover/views.py
+++ discover/views.py
@@ -1,26 +1,16 @@
-from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
-from rest_framework import viewsets, mixins, status
-from rest_framework import filters
-from rest_framework import permissions
+from rest_framework import viewsets
 from models import Scan, SpeedTest
 from connect.models import Gateway
-from django.http import Http404
-
-import json
 import serializers
 import logging
 
 logger = logging.getLogger(__name__)
 
+
 # API VIEWS
 
-
-class ScanViewSet(viewsets.GenericViewSet,
-                            mixins.ListModelMixin,
-                            mixins.RetrieveModelMixin,
-                            mixins.UpdateModelMixin,
-                            mixins.CreateModelMixin,):
+class ScanViewSet(viewsets.ModelViewSet):
     """
     Scan
     retrieve:
@@ -46,20 +36,12 @@ class ScanViewSet(viewsets.GenericViewSet,
     queryset = Scan.objects.all()
     filter_fields = ('processed',)
 
-    def list(self, request, gateway_hostname=None):
-        """
-        get all tunnels for a gateway
-        :param request:
-        :param gateway_id: str id for the gateway
-        :return:
-        """
-        gateway = Gateway.objects.get(hostname=gateway_hostname)
+    def get_queryset(self, **kwargs):
+        gateway = Gateway.objects.get(hostname=self.kwargs['gateway_hostname'])
         # make sure user is authorized to use this gateway
-        print self.request.user.has_perm('connect.view_gateway', gateway)
         if self.request.user.has_perm('connect.view_gateway', gateway):
-            scans = Scan.objects.filter(gateway=gateway)
-            serializer = self.get_serializer(scans, many=True)
-            return Response(serializer.data)
+            tunnels = Scan.objects.filter(gateway=gateway)
+            return tunnels
         else:
             raise Http404
 
@@ -68,6 +50,7 @@ class ScanViewSet(viewsets.GenericViewSet,
         get tunnel details
         :param request:
         :param gateway_id:
+        :param pk
         :return:
         """
         scans = Scan.objects.filter(pk=pk, gateway=gateway_id)
@@ -80,12 +63,7 @@ class ScanViewSet(viewsets.GenericViewSet,
         serializer.save(gateway=gateway, user=self.request.user)
 
 
-
-class SpeedTestViewSet(viewsets.GenericViewSet,
-                            mixins.ListModelMixin,
-                            mixins.RetrieveModelMixin,
-                            mixins.UpdateModelMixin,
-                            mixins.CreateModelMixin,):
+class SpeedTestViewSet(viewsets.ModelViewSet):
     """
     retrieve:
         get details about a speedtest
@@ -106,35 +84,44 @@ class SpeedTestViewSet(viewsets.GenericViewSet,
         update a speedtest
     """
 
-
     serializer_class = serializers.SpeedTestSerializer
     queryset = SpeedTest.objects.all()
     filter_fields = ('processed',)
 
-    def list(self, request, gateway_hostname=None):
-        """
-        get all speedtests for a gateway
-        :param request:
-        :param gateway_id: str id for the gateway
-        :return:
-        """
-        gateway = Gateway.objects.get(hostname=gateway_hostname)
-        print gateway
+    def get_queryset(self, **kwargs):
+        gateway = Gateway.objects.get(hostname=self.kwargs['gateway_hostname'])
         # make sure user is authorized to use this gateway
-        print self.request.user.has_perm('connect.view_gateway', gateway)
         if self.request.user.has_perm('connect.view_gateway', gateway):
-            speedtests = SpeedTest.objects.filter(gateway=gateway)
-            print speedtests
-            serializer = self.get_serializer(speedtests, many=True)
-            return Response(serializer.data)
+            tunnels = SpeedTest.objects.filter(gateway=gateway)
+            return tunnels
         else:
             raise Http404
+
+    # def list(self, request, gateway_hostname=None):
+    #     """
+    #     get all speedtests for a gateway
+    #     :param request:
+    #     :param gateway_id: str id for the gateway
+    #     :return:
+    #     """
+    #     gateway = Gateway.objects.get(hostname=gateway_hostname)
+    #     print gateway
+    #     # make sure user is authorized to use this gateway
+    #     print self.request.user.has_perm('connect.view_gateway', gateway)
+    #     if self.request.user.has_perm('connect.view_gateway', gateway):
+    #         speedtests = SpeedTest.objects.filter(gateway=gateway)
+    #         print speedtests
+    #         serializer = self.get_serializer(speedtests, many=True)
+    #         return Response(serializer.data)
+    #     else:
+    #         raise Http404
 
     def retrieve(self, request, gateway_hostname=None, pk=None):
         """
         get speedtest details
         :param request:
-        :param gateway_id:
+        :param gateway_hostname
+        :param pk:
         :return:
         """
         speedtests = SpeedTest.objects.filter(pk=pk, gateway=gateway_hostname)
@@ -145,79 +132,3 @@ class SpeedTestViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         gateway = Gateway.objects.get(hostname=self.request.data['gateway'])
         serializer.save(gateway=gateway, user=self.request.user)
-
-# class ScanViewSet(viewsets.ModelViewSet):
-#     """
-#     Scan
-#     retrieve:
-#         get a scan
-#
-#     list:
-#         get all scans
-#
-#     create:
-#         create a scan
-#
-#     delete:
-#         delete a scan
-#
-#     partial_update:
-#         update fields on a scan
-#
-#     update:
-#         update a scan
-#     """
-#     serializer_class = serializers.ScanSerializer
-#     filter_backends = (filters.DjangoFilterBackend,)
-#     filter_fields = ('processed', 'type',)
-#     permissions_classes = (permissions.IsAuthenticated,)
-#     lookup_field = 'id'
-#
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-#
-#     def get_queryset(self):
-#         """
-#         Return a list of all the gateways for the current user
-#         """
-#         user = self.request.user
-#         return Scan.objects.filter(user=user)
-#
-
-# class SpeedTestViewSet(viewsets.ModelViewSet):
-#     """
-#     retrieve:
-#         get a speed test
-#
-#     list:
-#         list all speed tests
-#
-#     create:
-#         create a speed test
-#
-#     delete:
-#         delete a speed test
-#
-#     partial_update:
-#         update fields on a speed test
-#
-#     update:
-#         update a speed test
-#     """
-#     serializer_class = serializers.SpeedTestSerializer
-#     filter_backends = (filters.DjangoFilterBackend,)
-#     filter_fields = ('processed', 'type',)
-#     permissions_classes = (permissions.IsAuthenticated,)
-#     lookup_field = 'id'
-#
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-#
-#     def get_queryset(self):
-#         """
-#         Return a list of all the gateways for the current user
-#         """
-#         user = self.request.user
-#         return SpeedTest.objects.filter(user=user)
-
-

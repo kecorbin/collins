78c1f5b505db1d81233af2e4e00c3abb5b5b7190
kecorbin <kecorbin@cisco.com>
2017-06-14 09:44:49 -0500

    discover admin tweaks

--- discover/admin.py
+++ discover/admin.py
@@ -4,12 +4,12 @@ from models import Scan, SpeedTest
 @admin.register(Scan)
 class ScanAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'gateway', 'type',
-                    'processed', 'created', 'modified')
+                    'processed', 'created',)
     search_fields = ('id', 'user__username', 'gateway')
 
 @admin.register(SpeedTest)
 class SpeedTestAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'gateway', 'processed',
-                    'created', 'modified')
+                    'created',)
     search_fields = ('id', 'user__username', 'gateway')
 
